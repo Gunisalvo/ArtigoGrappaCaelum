1a6aaa6b76bbe25364897e91bcb1f627247cf5f7
Rodrigo "Gunisalvo" Leite <player1@gunisalvo.com>
2014-06-15 08:39:37 -0300

    dispositivo

--- src/main/java/br/com/caelum/grappaExemplo/Aplicacao.java
+++ src/main/java/br/com/caelum/grappaExemplo/Aplicacao.java
@@ -1,29 +1,22 @@
 package br.com.caelum.grappaExemplo;
 
-import org.entrementes.grappa.ContextoGrappa;
-import org.entrementes.grappa.modelo.InstrucaoGrappa;
+import org.entrementes.grappa.contexto.ContextoDesktop;
+import org.entrementes.grappa.contexto.ContextoGrappa;
 import org.entrementes.grappa.modelo.instrucao.InstrucaoGPIO;
-import org.entrementes.grappa.modelo.instrucao.InstrucaoRegistrador;
+
+import br.com.caelum.grappaExemplo.servico.ControleAcesso;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		ContextoGrappa.construir();
-		lerAcessos();
-		sinalizarAcesso();
-		lerAcessos();
-	}
-
-	private static void lerAcessos() {
-		// InstrucaoGrappa requisicao = new InstrucaoGrappa(1, Conexao.REGISTRADOR,TipoAcao.LEITURA, null);
-		InstrucaoGrappa resposta = ContextoGrappa.processarInstrucao(new InstrucaoRegistrador().endereco(1).ler());
-		System.out.println(resposta.getValor().getCorpo()); // n entrada(s) autorizada(s).
-	}
-
-	private static void sinalizarAcesso() {
-		// InstrucaoGrappa requisicao = new InstrucaoGrappa(1, Conexao.GPIO,TipoAcao.ESCRITA, 1);
-		ContextoGrappa.processarInstrucao(new InstrucaoGPIO().endereco(2).escrever(1)); // Abriu
-		ContextoGrappa.processarInstrucao(new InstrucaoGPIO().endereco(2).escrever(0)); // Fechou
+		ContextoGrappa contexto = new ContextoDesktop();
+		ControleAcesso dispositivo = (ControleAcesso) contexto.getDispositivos().get("controle-acesso");
+		System.out.println(dispositivo.getContador() + " entrada(s) autorizada(s).");
+		// InstrucaoGrappa requisicao = new InstrucaoGrappa(2, Conexao.GPIO,TipoAcao.ESCRITA, 1);
+		contexto.processarInstrucao(new InstrucaoGPIO().endereco(2).escrever(1)); // Abriu
+		// InstrucaoGrappa requisicao = new InstrucaoGrappa(2, Conexao.GPIO,TipoAcao.ESCRITA, 0);
+		contexto.processarInstrucao(new InstrucaoGPIO().endereco(2).escrever(0)); // Fechou
+		System.out.println(dispositivo.getContador() + " entrada(s) autorizada(s).");
 	}
 
 }
--- src/main/java/br/com/caelum/grappaExemplo/servico/ControleAcesso.java
+++ src/main/java/br/com/caelum/grappaExemplo/servico/ControleAcesso.java
@@ -1,23 +1,71 @@
 package br.com.caelum.grappaExemplo.servico; // 1
 
-import org.entrementes.grappa.ContextoGrappa;
-import org.entrementes.grappa.ContextoGrappa.NivelLog;
-import org.entrementes.grappa.gpio.ObservadorGpio;
-import org.entrementes.grappa.gpio.ServicoGpio;
-import org.entrementes.grappa.modelo.instrucao.InstrucaoRegistrador;
+import java.util.Properties;
 
-@ObservadorGpio(endereco = 2)// 2
-public class ControleAcesso implements ServicoGpio { // 3
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.entrementes.grappa.dispositivo.Dispositivo;
+import org.entrementes.grappa.gpio.ObservadorGpio;
+
+@Dispositivo(nome = "controle-acesso")// 2
+public class ControleAcesso {
 
 	private Integer contador = 0;
+	
+	private final String USUARIO_GMAIL = "seu usuario Gmail";
+	private final String SENHA_GMAIL = "sua senha Gmail";
+	private final String EMAIL_GMAIL = "seu email Gmail";
+	private final String EMAIL_DESTINO = "email notificado";
 
-	@Override
-	public void processarServico(Integer estadoPino) { // 4
-		ContextoGrappa.getAplicacao().log("Evento pino - estado : " + estadoPino, NivelLog.INFO); // 5
-		if (estadoPino.intValue() == 1) { // 6
+	public Integer getContador() {
+		return contador;
+	}
+	
+	@ObservadorGpio(endereco = 2)
+	public void processarServico(Integer estadoPino) { // 3
+		System.out.println("processando evento eletrico.");
+		
+		if (estadoPino.intValue() == 1) { // 4
 			this.contador += 1;
-			String entradas = this.contador + " entrada(s) autorizada(s).";
-			ContextoGrappa.processarInstrucao(new InstrucaoRegistrador().endereco(1).escrever(entradas)); // 7
+			String mensagem = this.contador + " entrada(s) autorizada(s).";
+			enviarEmailPeloGmail(mensagem); //5
 		}
 	}
+	
+	private void enviarEmailPeloGmail(String corpoMensagem) {
+		System.out.println("enviando mensagem.");
+		
+		Properties configuracaoGmail = new Properties();
+		configuracaoGmail.put("mail.smtp.host", "smtp.gmail.com");
+		configuracaoGmail.put("mail.smtp.socketFactory.port", "465");
+		configuracaoGmail.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		configuracaoGmail.put("mail.smtp.auth", "true");
+		configuracaoGmail.put("mail.smtp.port", "465");
+
+		Session sessaoGmail = Session.getDefaultInstance(configuracaoGmail,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(USUARIO_GMAIL,SENHA_GMAIL);
+					}
+				});
+		try {
+			Message emailNotificacao = new MimeMessage(sessaoGmail);
+			emailNotificacao.setFrom(new InternetAddress(EMAIL_GMAIL));
+			emailNotificacao.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(EMAIL_DESTINO));
+			emailNotificacao.setSubject("[Grappa] Evento Registrado!");
+			emailNotificacao.setText(corpoMensagem);
+			Transport.send(emailNotificacao);
+
+		} catch (MessagingException ex) {
+			throw new RuntimeException(ex);
+		}
+	}	
 }
