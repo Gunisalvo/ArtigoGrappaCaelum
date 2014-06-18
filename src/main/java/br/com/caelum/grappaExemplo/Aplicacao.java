baf1f822b1c6a417de6e35e7e70c4ba2b9c2fd52
Rodrigo "Gunisalvo" Leite <player1@gunisalvo.com>
2014-06-18 15:39:42 -0300

    final

--- src/main/java/br/com/caelum/grappaExemplo/Aplicacao.java
+++ src/main/java/br/com/caelum/grappaExemplo/Aplicacao.java
@@ -1,25 +1,21 @@
 package br.com.caelum.grappaExemplo;
 
-import org.entrementes.grappa.contexto.ContextoDesktop;
 import org.entrementes.grappa.contexto.ContextoGrappa;
+import org.entrementes.grappa.contexto.ContextoPadrao;
 import org.entrementes.grappa.gpio.Raspberry;
-import org.entrementes.grappa.modelo.instrucao.InstrucaoDigital;
+import org.entrementes.grappa.modelo.InstrucaoGrappa;
+import org.entrementes.grappa.modelo.InstrucaoGrappa.Acao;
+import org.entrementes.grappa.modelo.InstrucaoGrappa.Formato;
+import org.entrementes.grappa.modelo.instrucao.InstrucaoLogica;
 
 public class Aplicacao {
-
-	private static Raspberry PI;
 	
 	public static void main(String[] args) {
-		iniciarHardware();
-		// InstrucaoGrappa requisicao = new InstrucaoGrappa(2, Conexao.GPIO,TipoAcao.ESCRITA, 1);
-		PI.processarInstrucao(new InstrucaoDigital().endereco(2).escrever(1)); // Abriu
-		// InstrucaoGrappa requisicao = new InstrucaoGrappa(2, Conexao.GPIO,TipoAcao.ESCRITA, 0);
-		PI.processarInstrucao(new InstrucaoDigital().endereco(2).escrever(0)); // Fechou
-	}
-	
-	public static void iniciarHardware(){
-		ContextoGrappa contexto = new ContextoDesktop();
-		PI = contexto.getImplementacao();
+		ContextoGrappa contexto = new ContextoPadrao();
+		Raspberry pi = contexto.getImplementacao();
+		InstrucaoGrappa requisicao = new InstrucaoGrappa(2, Formato.LOGICO, Acao.ESCRITA, 1);
+		pi.processarInstrucao(requisicao); // Abriu
+		pi.processarInstrucao(new InstrucaoLogica().endereco(2).escrever(0)); // Fechou
 	}
 
 }
--- src/main/java/br/com/caelum/grappaExemplo/servico/ControleAcesso.java
+++ src/main/java/br/com/caelum/grappaExemplo/servico/ControleAcesso.java
@@ -1,15 +1,5 @@
 package br.com.caelum.grappaExemplo.servico; // 1
 
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.entrementes.grappa.gpio.ServicoGpio;
 import org.entrementes.grappa.marcacao.ObservadorGpio;
 
@@ -18,11 +8,6 @@ public class ControleAcesso implements ServicoGpio{
 
 	private Integer contador = 0;
 	
-	private final String USUARIO_GMAIL = "seu usuario Gmail";
-	private final String SENHA_GMAIL = "sua senha Gmail";
-	private final String EMAIL_GMAIL = "seu email Gmail";
-	private final String EMAIL_DESTINO = "email notificado";
-	
 	@Override
 	public void processarServico(Integer estadoPino) { // 3
 		System.out.println("processando evento eletrico.");
@@ -30,38 +15,7 @@ public class ControleAcesso implements ServicoGpio{
 		if (estadoPino.intValue() == 1) { // 4
 			this.contador += 1;
 			String mensagem = this.contador + " entrada(s) autorizada(s).";
-			enviarEmailPeloGmail(mensagem); //5
+			System.out.println(mensagem); //5
 		}
 	}
-	
-	private void enviarEmailPeloGmail(String corpoMensagem) {
-		System.out.println("enviando mensagem.");
-		
-		Properties configuracaoGmail = new Properties();
-		configuracaoGmail.put("mail.smtp.host", "smtp.gmail.com");
-		configuracaoGmail.put("mail.smtp.socketFactory.port", "465");
-		configuracaoGmail.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		configuracaoGmail.put("mail.smtp.auth", "true");
-		configuracaoGmail.put("mail.smtp.port", "465");
-
-		Session sessaoGmail = Session.getDefaultInstance(configuracaoGmail,
-				new javax.mail.Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(USUARIO_GMAIL,SENHA_GMAIL);
-					}
-				});
-		try {
-			Message emailNotificacao = new MimeMessage(sessaoGmail);
-			emailNotificacao.setFrom(new InternetAddress(EMAIL_GMAIL));
-			emailNotificacao.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse(EMAIL_DESTINO));
-			emailNotificacao.setSubject("[Grappa] Evento Registrado!");
-			emailNotificacao.setText(corpoMensagem);
-			Transport.send(emailNotificacao);
-
-		} catch (MessagingException ex) {
-			throw new RuntimeException(ex);
-		}
-	}	
 }
